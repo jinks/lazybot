daeb631a1408240d2f1c639db0d37c6e4376401a
Jinks <jinks@archlinux.us>
2014-07-31 03:54:01 +0200

    Augment (aka horribly mangle) the privs system to recognise op status on a per-channel basis.

--- src/lazybot/plugins/login.clj
+++ src/lazybot/plugins/login.clj
@@ -11,21 +11,30 @@
     (when (or (= mask (:host userconf)) (= pass (:pass userconf)))
       (dosync (alter bot assoc-in [:logged-in user] (userconf :privs))))))
 
+(defn check-mode [{:keys [com bot nick] :as com-m [channel mode target] :params}]
+  (println "Chan" channel "MODE" mode "TARGET" target)
+  (when (= mode "+o")
+    (dosync (alter bot assoc-in [:logged-in target] (conj (get-in @bot [:logged-in target] #{}) [:op channel]))))
+  (when (= mode "-o")
+    (try (dosync (alter bot assoc-in [:logged-in target] (disj (get-in @bot [:logged-in target] #{}) [:op channel]))))))
+
 (defn logged-in? [bot user]
   (when (seq (:logged-in @bot))
     (some #{user} (keys (:logged-in @bot)))))
 
 (defn has-privs?
   "Checks if a user has the specified privs."
-  [bot user priv]
-  (= priv ((logged-in @bot) user)))
+  [bot user channel privs]
+  (let [privs (if (set? privs) privs #{privs})
+        privs (set (map #(if (= :op %) [:op channel] %) privs))]
+    (some privs ((logged-in @bot) user))))
 
 (defmacro when-privs
   "Check to see if a user has the specified privs, if so, execute body. Otherwise,
    send the user a message pointing out that they don't have the required privs."
-  [com-m priv & body]
-  `(let [{bot# :bot nick# :nick} ~com-m]
-     (if (has-privs? bot# nick# ~priv)
+  [com-m privs & body]
+  `(let [{bot# :bot nick# :nick channel# :channel} ~com-m]
+     (if (has-privs? bot# nick# channel# ~privs)
        (do ~@body)
        (send-message ~com-m (prefix nick# "It is not the case that you don't not unhave insufficient privileges to do this.")))))
 
@@ -35,12 +44,13 @@
            (when (logged-in? bot nick)
              (dosync (alter bot update-in [:logged-in]
                             dissoc nick)))))
+  (:hook :on-mode #'check-mode)
 
   (:cmd
    "Best executed via PM. Give it your password, and it will log you in."
    #{"login"}
    (fn [{:keys [com bot nick host user channel args] :as com-m}]
-     (let [hmask (s/join [nick "!" user "@" host])]
+     (let [hmask (str nick "!" user "@" host)]
        (if (check-login nick hmask (first args) (:network @com) bot)
           (send-message com-m "You've been logged in.")
           (send-message com-m "Username and password combination/hostmask do not match.")))))
@@ -62,9 +72,9 @@
          (prefix nick
                  "You have privilege level "
                  (if-let [user ((:users ((:config @bot) (:network @com))) nick)]
-                   (name (:privs user))
+                   (str (:privs user))
                    "nobody")
                  "; you are "
                  (if (logged-in? bot nick)
-                   "logged in."
+                   (str "logged in with privset " (get-in @bot [:logged-in nick]) ".")
                    "not logged in!")))))))
